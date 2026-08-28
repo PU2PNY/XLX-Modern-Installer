d028424b8f2a1688c27705c8d6e4996b429b731f
PU2PNY <47339656+PU2PNY@users.noreply.github.com>
2026-08-27 23:48:28 -0300

    Build dashboard modules from the selected XLXD range

--- dashboard/config.php
+++ dashboard/config.php
@@ -1,4 +1,28 @@
 <?php
+declare(strict_types=1);
+
+/*
+ * The server installer records the number of enabled XLX modules in site.php.
+ * Build the dashboard module list from that value so installations can use
+ * any contiguous range from A through Z without editing this file.
+ */
+$site = require __DIR__ . '/config/site.php';
+$moduleCount = (int)($site['radio']['module_count'] ?? 5);
+$moduleCount = max(1, min(26, $moduleCount));
+$ysfId = (string)($site['radio']['ysf_id'] ?? '');
+
+$modules = [];
+
+for ($index = 0; $index < $moduleCount; $index++) {
+    $letter = chr(65 + $index);
+    $modules[$letter] = [
+        'name' => 'Module ' . $letter,
+        'protocol' => 'D-STAR / DMR / C4FM-YSF',
+        'access' => 'DMR TG ' . (4001 + $index)
+            . ($ysfId !== '' ? ' • YSF ' . $ysfId : ''),
+    ];
+}
+
 return [
   'server_name' => '{{REFLECTOR_TITLE}}',
   'xml_path' => '/var/log/xlxd.xml',
@@ -6,11 +30,5 @@ return [
   'users_db' => '/xlxd/users_db/users.db',
   'users_override_db' => '/var/lib/xlx-user-directory/overrides.db',
   'history_limit' => 30,
-  'modules' => [
-    'A' => ['name'=>'DMR','protocol'=>'DMR','access'=>'TG 4001'],
-    'B' => ['name'=>'APRS / D-PRS','protocol'=>'APRS / D-PRS','access'=>'Dados digitais'],
-    'C' => ['name'=>'C4FM / YSF / DMR','protocol'=>'C4FM/YSF e DMR','access'=>'YSF {{YSF_ID}} • DMR TG {{DMR_TG}}'],
-    'D' => ['name'=>'D-STAR','protocol'=>'D-STAR','access'=>'{{REFLECTOR_NAME}}-D / XRF{{REFLECTOR_NUMBER}}-D'],
-    'E' => ['name'=>'Teste / Echo','protocol'=>'D-STAR Echo','access'=>'Teste de áudio'],
-  ],
+  'modules' => $modules,
 ];
